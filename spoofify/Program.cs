8011ee73d6a1064f2d32f8bc74871b12743bd663
Charlie Gadd <gaddc11@gmail.com>
2022-07-24 04:18:06 +1000

    Fixed a weird mistake.

--- spoofify/Program.cs
+++ spoofify/Program.cs
@@ -1,14 +1,14 @@
 ﻿using SpotifyAPI.Web;
 
 // Get token from https://developer.spotify.com/console/post-playlist-tracks/
-string Token = "";
+string Token = "BQA5ebG7Y3jMUAO6ObZKTOpNdSlf6ma_Dr2rQH5-PADsePmUUAKtwk3gv1lY23IOCn7FwHZHkBAxuWEzeQ7uH7IwlXgx5-2ncZQzl8uUCWNbQptr7uqqapD6DDjbICB-AQbAQKvnYcrXYfvaRMVk-kRUcCEF3SfeghEiGWE4xUlsHZSg5sWvS6FNlJp_P_HkKSel6vaY_YdAbhm-xSLlp80-xHWXa-iY8-S61Y6uS9kXsFs_";
 
 // Get market code from https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-string Market = "";
+string Market = "AU";
 
 // Specify ID's here:
-string Playlist = "";
-string Artist = "";
+string Playlist = "6uYFrPYkbFDOlevOf8KqJk";
+string Artist = "7wqtxqI3eo7Gn1P7SpP6cQ"; 
 
 // The API client itself.
 SpotifyClient Client = new(Token);
@@ -63,7 +63,7 @@ if (albums.Total > 0 && albums.Items != null)
                     {
                         Console.WriteLine($" {album.TotalTracks} track{S(album.TotalTracks)} found but list is null. (wtf?!?!?!?)");
                     }
-
+ 
                     trackOffset += 50;
                 }
             }
@@ -80,7 +80,7 @@ if (albums.Total > 0 && albums.Items != null)
     Console.WriteLine();
 
     int songOffset = 0;
-    while (albumOffset < songs.Count)
+    while (songOffset < songs.Count)
     {
         Console.WriteLine($"{songOffset}/{songs.Count}");
         await AddSongs(songs.Subset(songOffset, 100));
